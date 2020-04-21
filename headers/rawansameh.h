351651e72b23ecf4f1a649384297c32ad4a8ac28
Rawan Hamad <samehsrrj@aucegypt.edu>
2020-04-21 11:47:01 +0200

    Add files to branch

---
+++ headers/rawansameh.h
@@ -0,0 +1,6 @@
+#ifndef rawansameh_h
+#define rawansameh_h
+#include <iostream> 
+using namespace std;
+void rawansameh();
+#endif
---
+++ sources/rawansameh.cpp
@@ -0,0 +1,4 @@
+#include "rawansameh.h"
+void rawansameh(){
+cout << "Name: Rawan Hamad" << endl;
+cout << "Email: samehsrrj@aucegypt.edu" << endl; }
